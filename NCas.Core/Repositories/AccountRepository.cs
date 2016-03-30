b6323ff1d9246ed129aa5403f1e40c4a1b79547a
virtual <1185513330@qq.com>
2016-03-30 08:53:11 +0800

    repositories Signed-off-by: virtual <1185513330@qq.com>

---
+++ NCas.Core/Repositories/AccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCas.Core.Repositories
+{
+    /// <summary>账号仓储
+    /// </summary>
+    public class AccountRepository : IAccountRepository
+    {
+
+    }
+}
---
+++ NCas.Core/Repositories/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCas.Core.Repositories
+{
+    /// <summary>账号仓储接口
+    /// </summary>
+    public interface IAccountRepository
+    {
+    }
+}
